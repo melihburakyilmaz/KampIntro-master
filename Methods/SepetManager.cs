47425b66e437ce3977b7de7375735ee8a4f9f1f6
Melih Burak Yılmaz <200705059@kku.edu.tr>
2021-01-16 23:56:11 +0300

    .

---
+++ Methods/SepetManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class SepetManager
+    {
+        //naming convention
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tepbrikler ! Sepete Eklendi : "+ urun.Adi);
+
+
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int StokAdedi)
+        {
+            Console.WriteLine(("Tepbrikler ! Sepete Eklendi : " + urunAdi);
+        }
+
+    }
+}
